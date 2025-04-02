9593bba18c500ef9687319cfb76519a7b76acf30
Msithiphong <Csithiphonggg@gmail.com>
2025-04-01 21:23:26 -0700

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { StickyNavbar } from './components/StickyNavbar';
 import './index.css'
 
 function App() {
   return (
-    //<div className="min-h-screen bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 animate-gradient text-white flex items-center justify-center">
-      //<h1 className="text-5xl font-bold">Hello my name is Maxwell Sithiphong</h1>
-   // </div>
-  
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black flex items-center justify-center">
-  <div className="bg-white/10 backdrop-blur-md rounded-xl p-10 border border-white/30 shadow-lg text-white">
-    <h1 className="text-3xl font-semibold">Glassmorphism Card</h1>
-    <p className="text-sm mt-2">Looks frosty ❄️</p>
-  </div>
-  </div>
+    <>
+      <StickyNavbar />
+      <div className="min-h-screen bg-gradient-to-br from-gray-900 to-black flex items-center justify-center">
+        <div className="bg-white/10 backdrop-blur-md rounded-xl p-10 border border-white/30 shadow-lg text-white">
+          <h1 className="text-3xl font-semibold">
+            Maxwell Sithiphong's Portfolio Website
+          </h1>
+          <p className="text-sm mt-2">
+            Looks frosty ❄️
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
