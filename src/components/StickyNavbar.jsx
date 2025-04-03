fd52ed607e0ea120a91ab1ab83daf97947cb7cd3
Msithiphong <Csithiphonggg@gmail.com>
2025-04-02 19:53:50 -0700

    flex

--- src/components/StickyNavbar.jsx
+++ src/components/StickyNavbar.jsx
@@ -76,7 +76,7 @@ export function StickyNavbar() {
             <img
               src="/images/headshot.png" // Ensure the file exists in public/images
               alt="Profile"
-              className="w-20 h-20 rounded-full object-cover hover-expand-effect" // Added zoom effect
+              className="flex w-20 h-20 rounded-full object-cover hover-expand-effect" // Added zoom effect
             />
             <Typography
               as="a"
